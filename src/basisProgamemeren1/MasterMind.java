2c5465aaa4cc91f2c0ab4aac6081b8883a91a2ba
fabricebexkens <1225790@student.roc-nijmegen.nl>
2026-09-16 11:13:32 +0200

    added variable for pins
    added pins 9 diffrent pins

--- src/basisProgamemeren1/MasterMind.java
+++ src/basisProgamemeren1/MasterMind.java
@@ -4,7 +4,18 @@ public class MasterMind {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String PinZwart = "zwart";
+		String PinWit = "wit";
+		
+		String PinRood = "rood";
+		String PinGroen = "groen";
+		String PinGeel = "geel";
+		String PinPaars = "paars";
+		String PinBlauw = "blauw";
+		String PinOranje = "oranje";
+		
+		String PinLeeg = "leeg";
 	}
 
 }
