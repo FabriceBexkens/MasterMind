431a16d1675b52eec9fffa4525ce49ecb70d03bf
fabricebexkens <1225790@student.roc-nijmegen.nl>
2026-09-18 16:14:25 +0200

    added scanner
    added scanner until row1

--- src/basisProgamemeren1/MasterMind.java
+++ src/basisProgamemeren1/MasterMind.java
@@ -1,9 +1,13 @@
 package basisProgamemeren1;
 
+import java.util.Scanner;
+
 public class MasterMind {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		
 		
 		String PinZwart = "zwart";
 		String PinWit = "wit";
@@ -17,10 +21,10 @@ public class MasterMind {
 		
 		String PinLeeg = "leeg";
 		
-		String codemakervak1;
-		String codemakervak2;
-		String codemakervak3;
-		String codemakervak4;
+		String CodemakerVak1 = "geel";
+		String CodemakerVak2 = "rood";
+		String CodemakerVak3 = "groen";
+		String CodemakerVak4 = "blauw";
 		
 		String Rij1Vak1;
 		String Rij1Vak2;
@@ -103,6 +107,65 @@ public class MasterMind {
 		String Rij10Checkvak2;
 		String Rij10Checkvak3;
 		String Rij10Checkvak4;
-	}
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("voer pin 1 in");
+		String Rij1vak1 = sc.next();
+		System.out.println("voer pin 2 in");
+		String Rij1vak2 = sc.next();
+		System.out.println("voer pin 3 in");
+		String Rij1vak3 = sc.next();
+		System.out.println("voor pin 4 in");
+		String Rij1vak4 = sc.next();
+		
+		
+		// Controle vak 1
+		if (Rij1vak1.equals(CodemakerVak1)) {
+		    Rij1Checkvak1 = "zwart";
+		} else if (Rij1vak1.equals(CodemakerVak2) ||
+		           Rij1vak1.equals(CodemakerVak3) ||
+		           Rij1vak1.equals(CodemakerVak4)) {
+		    Rij1Checkvak1 = "wit";
+		} else {
+		    Rij1Checkvak1 = "leeg";
+		}
 
+		// Controle vak 2
+		if (Rij1vak2.equals(CodemakerVak2)) {
+		    Rij1Checkvak2 = "zwart";
+		} else if (Rij1vak2.equals(CodemakerVak1) ||
+		           Rij1vak2.equals(CodemakerVak3) ||
+		           Rij1vak2.equals(CodemakerVak4)) {
+		    Rij1Checkvak2 = "wit";
+		} else {
+		    Rij1Checkvak2 = "leeg";
+		}
+
+		// Controle vak 3
+		if (Rij1vak3.equals(CodemakerVak3)) {
+		    Rij1Checkvak3 = "zwart";
+		} else if (Rij1vak3.equals(CodemakerVak1) ||
+		           Rij1vak3.equals(CodemakerVak2) ||
+		           Rij1vak3.equals(CodemakerVak4)) {
+		    Rij1Checkvak3 = "wit";
+		} else {
+		    Rij1Checkvak3 = "leeg";
+		}
+
+		if (Rij1vak4.equals(CodemakerVak4)) {
+		    Rij1Checkvak4 = "zwart";
+		} else if (Rij1vak4.equals(CodemakerVak1) ||
+		           Rij1vak4.equals(CodemakerVak2) ||
+		           Rij1vak4.equals(CodemakerVak3)) {
+		    Rij1Checkvak4 = "wit";
+		} else {
+		    Rij1Checkvak4 = "leeg";
+		}
+
+		// PRINTEN!
+		System.out.println("antwoord:");
+		System.out.println(Rij1Checkvak1 + " " + Rij1Checkvak2 + " " + Rij1Checkvak3 + " " + Rij1Checkvak4);
+
+
+	}
 }
